b93e23909e51d539030debbdfa4bb8343b96acf0
Matt Mowris <matt.mowris@gmail.com>
2017-02-08 10:31:28 -0800

    test

---
+++ index.js
@@ -0,0 +1,59 @@
+var admin = require('firebase-admin'),
+	serviceaccount = require('key.json'),
+	request = require('request');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceaccount),
+  databaseURL: "https://mmdh-57486.firebaseio.com"
+});
+
+// Initialize the app with a service account, granting admin privileges
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://<your database>.firebaseio.com/"
+});
+ref = firebase.database().ref();
+
+function listenForNotificationRequests() {
+  var requests = ref.child('notificationRequests');
+  requests.on('child_added', function(requestSnapshot) {
+    var request = requestSnapshot.val();
+    sendNotificationToUser(
+      request.username, 
+      request.message,
+      function() {
+        requestSnapshot.ref.remove();
+      }
+    );
+  }, function(error) {
+    console.error(error);
+  });
+};
+
+function sendNotificationToUser(username, message, onSuccess) {
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type' :' application/json',
+      'Authorization': 'key='+process.env.API_KEY
+    },
+    body: JSON.stringify({
+      notification: {
+        title: message
+      },
+      to : '/topics/user_'+username
+    })
+  }, function(error, response, body) {
+    if (error) { console.error(error); }
+    else if (response.statusCode >= 400) { 
+      console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+    }
+    else {
+      onSuccess();
+    }
+  });
+}
+
+// start listening
+listenForNotificationRequests();
